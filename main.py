8205a530c78cdf5820e2ea4c84cba0ce4e9233b1
Koen Ponse <k.ponse@liacs.leidenuniv.nl>
2025-10-29 17:28:52 +0100

    adjust example main file for reasonable training results

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+import argparse
+
 import jax
 import jaxnasium as jym
 import numpy as np
-from jaxnasium.algorithms import PPO
+from jaxnasium.algorithms import DQN, PPO, SAC
 
 from chargax import Chargax, get_electricity_prices  # noqa: E402
 
@@ -14,21 +16,18 @@ if __name__ == "__main__":
     rng = jax.random.PRNGKey(42)
 
     # RL Training with PPO
-    num_envs = 4
-    num_steps = 300
     total_timesteps = 1_000_000
-    num_epochs = 4
-    num_training_iterations = (total_timesteps // num_steps // num_envs) * num_epochs
     agent = PPO(  # Not optimized, just a simple example
-        num_steps=num_steps,
-        num_envs=num_envs,
-        num_epochs=num_epochs,
+        num_steps=300,
+        num_envs=8,
         total_timesteps=total_timesteps,
-        learning_rate=2.5e-3,
+        learning_rate=2.5e-4,
         anneal_learning_rate=True,
+        normalize_rewards=False,
+        normalize_observations=True,  # Important
     )
 
-    agent: PPO = agent.train(rng, env)
+    agent = agent.train(rng, env)
 
     results = agent.evaluate(rng, env, num_eval_episodes=25)
     print(f"Average reward over 25 evaluation episodes: {np.mean(results)}")
